d71963c7d5f5f0eb270a3f86a9897ed3625d456f
Anastasia_Brikun <bri.anastasia.work@gmail.com>
2020-11-24 21:36:33 +0300

    Second task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,9 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
-}
+  if ( str.length > maxlength) {
+    return str.substr(0, maxlength - 1) + '…';
+  } else {
+      return str;
+  }
+};
